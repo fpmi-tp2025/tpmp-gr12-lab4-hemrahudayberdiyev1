c7ffffb065d0557b2b05c6f89a26f095cfef370a
Hemra Hudayberdiyev <hhudayberdiyew411@gmail.com>
2025-04-15 22:14:57 +0300

    Update

--- includes/auth.h
+++ includes/auth.h
@@ -1,9 +1,6 @@
 #ifndef AUTH_H
 #define AUTH_H
 
-#include <sqlite3.h>
-
-int authenticate_user(sqlite3 *db, const char *username, const char *password);
-int register_user(sqlite3 *db, const char *username, const char *password);
+int authenticate(const char *username, const char *password);
 
 #endif
