2af4da5d5f9859afb90cbe86a5bc8a1918d2ab98
Hemra Hudayberdiyev <hhudayberdiyew411@gmail.com>
2025-04-15 21:39:00 +0300

    Update

--- includes/country.h
+++ includes/country.h
@@ -1,8 +1,14 @@
 #ifndef COUNTRY_H
 #define COUNTRY_H
 
-void add_country(sqlite3* db, const char* name, const char* capital, const char* language,
-                 int population, float square, const char* currency, const char* head);
-void delete_country(sqlite3* db, int id);
+#include <sqlite3.h>
+
+void add_country(sqlite3 *db, const char *name, const char *capital, const char *language,
+                 int population, float square, const char *currency, const char *head);
+void delete_country(sqlite3 *db, int id);
+void list_countries(sqlite3 *db);
+void list_regions_by_country(sqlite3 *db, const char *country_name);
+void avg_population_by_country(sqlite3 *db, const char *country_name);
+void total_population(sqlite3 *db);
 
 #endif
