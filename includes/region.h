ca213a1c2661f4b18f32b12ef61013899f16d5c3
Hemra Hudayberdiyev <hhudayberdiyew411@gmail.com>
2025-04-15 21:43:10 +0300

    Update

--- includes/region.h
+++ includes/region.h
@@ -1,8 +1,11 @@
 #ifndef REGION_H
 #define REGION_H
 
-void add_region(sqlite3* db, const char* name, const char* capital, int population,
+#include <sqlite3.h>
+
+void add_region(sqlite3 *db, const char *name, const char *capital, int population,
                 float square, int country_id);
-void delete_region(sqlite3* db, int id);
+void delete_region(sqlite3 *db, int id);
+void list_regions(sqlite3 *db);
 
 #endif
