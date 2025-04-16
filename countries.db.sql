43eb2a3d2eddfa38b5d84df11b5862975f36e6e8
hemrahudayberdiyev1 <hemra7351@gmail.com>
2025-04-16 09:44:01 +0300

    Update countries.db.sql

--- countries.db.sql
+++ countries.db.sql
@@ -1,45 +1,60 @@
--- Создание таблиц
+-- countries.sql
+-- Создание таблицы country
 CREATE TABLE country (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(100) NOT NULL,
-    capital VARCHAR(100),
-    language VARCHAR(50),
-    population_country INTEGER,
-    square_country REAL,
-    currency VARCHAR(50),
-    head_country VARCHAR(100)
+    capital VARCHAR(100) NOT NULL,
+    language VARCHAR(50) NOT NULL,
+    population_country INTEGER NOT NULL,
+    square_country FLOAT NOT NULL,
+    currency VARCHAR(50) NOT NULL,
+    head_country VARCHAR(100) NOT NULL
 );
 
+-- Создание таблицы region
 CREATE TABLE region (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(100) NOT NULL,
-    capital_region VARCHAR(100),
-    population_region INTEGER,
-    square_region REAL,
-    country_id INTEGER,
+    capital_region VARCHAR(100) NOT NULL,
+    population_region INTEGER NOT NULL,
+    square_region FLOAT NOT NULL,
+    country_id INTEGER NOT NULL,
     FOREIGN KEY (country_id) REFERENCES country(id) ON DELETE CASCADE
 );
 
-CREATE TABLE user (
+-- Создание таблицы User
+CREATE TABLE User (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
     password_hash VARCHAR(128) NOT NULL
 );
 
--- Наполнение данными
-INSERT INTO user (username, password_hash)
-VALUES ('admin', 'admin123'),
-       ('user1', 'user123');
+-- Вставка данных в таблицу country
+INSERT INTO country (name, capital, language, population_country, square_country, currency, head_country) VALUES
+('Беларусь', 'Минск', 'Белорусский', 9400000, 207600, 'Белорусский рубль', 'Александр Лукашенко'),
+('Россия', 'Москва', 'Русский', 146000000, 17125191, 'Российский рубль', 'Владимир Путин'),
+('Туркменистан', 'Ашхабад', 'Туркменский', 6100000, 488100, 'Туркменский манат', 'Сердар Бердымухамедов'),
+('Турция', 'Анкара', 'Турецкий', 85000000, 783562, 'Турецкая лира', 'Реджеп Тайип Эрдоган');
 
-INSERT INTO country (name, capital, language, population_country, square_country, currency, head_country)
-VALUES ('Belarus', 'Minsk', 'Belarusian, Russian', 9400000, 207600.0, 'BYN', 'President Lukashenko'),
-       ('China', 'Beijing', 'Mandarin', 1400000000, 9596961.0, 'CNY', 'President Xi Jinping'),
-       ('Russia', 'Moscow', 'Russian', 146000000, 17125191.0, 'RUB', 'President Putin');
+-- Вставка данных в таблицу region
+INSERT INTO region (name, capital_region, population_region, square_region, country_id) VALUES
+-- Беларусь (country_id = 1)
+('Минская область', 'Минск', 1400000, 39854, 1),
+('Гродненская область', 'Гродно', 1000000, 25127, 1),
+('Брестская область', 'Брест', 1300000, 32786, 1),
+-- Россия (country_id = 2)
+('Московская область', 'Красногорск', 7800000, 44329, 2),
+('Ленинградская область', 'Гатчина', 1900000, 83908, 2),
+('Свердловская область', 'Екатеринбург', 4300000, 194307, 2),
+-- Туркменистан (country_id = 3)
+('Ахалский велаят', 'Анев', 900000, 97300, 3),
+('Балканский велаят', 'Балканабат', 500000, 139300, 3),
+('Дашогузский велаят', 'Дашогуз', 1400000, 73500, 3),
+-- Турция (country_id = 4)
+('Стамбул', 'Стамбул', 15500000, 5343, 4),
+('Измир', 'Измир', 4400000, 11891, 4),
+('Анталья', 'Анталья', 2600000, 20723, 4);
 
-INSERT INTO region (name, capital_region, population_region, square_region, country_id)
-VALUES ('Minsk Region', 'Minsk', 1400000, 39800.0, 1),
-       ('Gomel Region', 'Gomel', 1400000, 40400.0, 1),
-       ('Guangdong', 'Guangzhou', 113000000, 179800.0, 2),
-       ('Sichuan', 'Chengdu', 83400000, 485000.0, 2),
-       ('Moscow Region', 'Moscow', 7500000, 44300.0, 3),
-       ('Leningrad Region', 'Gatchina', 1800000, 83900.0, 3);
+-- Вставка тестового пользователя в таблицу User
+INSERT INTO User (username, password_hash) VALUES
+('admin', 'admin');
